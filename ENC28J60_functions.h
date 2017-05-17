92324ec180d565c713b5269977e792af57622eb9
LesserWorks <judahlesser@hotmail.com>
2017-05-17 14:55:49 -0600

    Update ENC28J60_functions.h

--- ENC28J60_functions.h
+++ ENC28J60_functions.h
@@ -1,2 +1,5 @@
+#ifndef ENC28J60_FUNCTIONS_H // Inlcude guard
+#define ENC28J60_FUNCTIONS_H
 extern void WriteReg(uint8_t registerName, uint8_t data);
 extern uint8_t ReadReg(uint8_t registerName);
+#endif /* ENC28J60_FUNCTIONS_H */
