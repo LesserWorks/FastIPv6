e7e29caaf4c3c871717642ba9c8d60ddb7699dab
LesserWorks <judahlesser@hotmail.com>
2017-05-17 11:54:50 -0600

    Update FastIPv6.h

--- FastIPv6.h
+++ FastIPv6.h
@@ -1,10 +1,14 @@
 /* Uncomment ONE of the following depending on what PHY interface you are using. */
 //#define USE_ENC28J60
 //#define USE_ENCX24J600
+#ifndef FASTIPv6_H
+#define FASTIPv6_H
+
 #ifdef USE_ENC28J60
 #include <ENC28J60_registers.h>
 #else
 #include <ENCX24J600_registers.h>
+#endif
 #endif
 
 
