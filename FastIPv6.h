e83971ba9fe94bf28e8c6b38f7da6221bb237d61
LesserWorks <judahlesser@hotmail.com>
2017-05-17 14:53:24 -0600

    Update FastIPv6.h

--- FastIPv6.h
+++ FastIPv6.h
@@ -1,16 +1,14 @@
-/* Uncomment ONE of the following depending on what PHY interface you are using. */
-//#define USE_ENC28J60
-//#define USE_ENCX24J600
 #ifndef FAST_IPv6_H // Include guard
 #define FAST_IPv6_H
+/* Uncomment ONE of the following depending on what ethernet controller you are using. */
+//#define USE_ENC28J60
+//#define USE_ENCX24J600
 
-#ifdef USE_ENC28J60
-#include <ENC28J60_registers.h>
-#else
-#include <ENCX24J600_registers.h>
-#endif /* USE_ENC28J60 */
-#endif /* FAST_IPv6_H */
+
+
 
 /* Functions visible to other files. */
 extern void solicitRouter(uint8_t registerName, uint8_t data);
 
+
+#endif /* FAST_IPv6_H */
