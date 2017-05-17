a49196d44e9bdb8659cdf0a315ae619e35cf3404
LesserWorks <judahlesser@hotmail.com>
2017-05-17 14:50:22 -0600

    Update FastIPv6.c

--- FastIPv6.c
+++ FastIPv6.c
@@ -1,6 +1,12 @@
 #include <FastIPv6.h>
-#include <ENC28J60_functions.h>
-void solicitRouter(void)
+#ifdef USE_ENC28J60
+  #include <ENC28J60_functions.h>
+  #include <ENC28J60_registers.h>
+#else
+  #include <ENCX24J600_registers.h>
+#endif /* USE_ENC28J60 */
+extern void solicitRouter(void) // Visible to other files
 {
   return;
 }
+// Put local functions with static prototype and function here.
