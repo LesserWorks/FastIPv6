b40ed8e2db770706bd7405826eefdbafc178c48f
LesserWorks <judahlesser@hotmail.com>
2017-05-17 11:50:40 -0600

    Update FastIPv6.h

--- FastIPv6.h
+++ FastIPv6.h
@@ -8,5 +8,5 @@
 #endif
 
 
-extern void WriteReg(uint8_t registerName, uint8_t data);
-extern uint8_t ReadReg(uint8_t registerName);
+extern void solicitRouter(uint8_t registerName, uint8_t data);
+
