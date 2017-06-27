86684a3982746e22c99443430c6a95e70490f7da
LesserWorks <judahlesser@hotmail.com>
2017-06-27 12:09:22 -0600

    Update ENCX24J600_functions.c

--- ENCX24J600_functions.c
+++ ENCX24J600_functions.c
@@ -1,15 +1,48 @@
-extern uint16_t ReadPHY(uint8_t PHYaddress)
+extern uint16_t ReadPHY(const uint8_t PHYaddress)
 {
-  indir(MIREGADRL) = PHYaddress;
+  while(indir(MISTATL) & (1 << BUSY)); // Wait for previous operation to finish
+  indir(MIREGADRL) = PHYaddress; // Load address
   indir(MIREGADRH) = 1U;
-  indir(MICMDL) = 1 << MIIRD;
+  indir(MICMDL) = 1 << MIIRD; // Start read
   indir(MICMDH) = 0; // Can this line be deleted?
-  while(indir(MISTATL) & (1 << BUSY));
-  indir(MICMDL) = 0;
+  while(indir(MISTATL) & (1 << BUSY)); // Wait for read to finish
+  indir(MICMDL) = 0; // Clear MIIRD
   indir(MICMDH) = 0;
-  return (((uint16_t)indir(MIIRDH)) << 8) | (indir(MIIRDL);
-                                             }
-                                             
+  return (((uint16_t)indir(MIRDH)) << 8) | indir(MIRDL); // Read result
+}
+
+extern void WritePHY(const uint8_t PHYaddress, const uint8_t PHYdatahigh, const uint8_t PHYdatalow)
+{
+  while(indir(MISTATL) & (1 << BUSY)); // Wait for previous operation to finish
+  indir(MIREGADRL) = PHYaddress; // Load address
+  indir(MIREGADRH) = 1U;
+  indir(MIWRL) = PHYdatalow; // Load data
+  indir(MIWRH) = PHYdatahigh; // Load data, automatically starts write operation
+  return;
+}
+
+extern void startPHYscan(const uint8_t PHYaddress)
+{
+  while(indir(MISTATL) & (1 << BUSY)); // Wait for previous operation to finish
+  indir(MIREGADRL) = PHYaddress; // Load address
+  indir(MIREGADRH) = 1U;
+  indir(MICMDL) = 1 << MIISCAN; // Start read
+  indir(MICMDH) = 0; // Can this line be deleted?
+  while(indir(MISTATL) & (1 << NVALID)); // Wait for first valid data
+  return;
+}
+
+extern uint16_t readPHYscan(void)
+{
+  return (((uint16_t)indir(MIRDH)) << 8) | indir(MIRDL); // Read registers
+}
+
+extern void stopPHYscan(void)
+{
+  indir(MICMDL) = 0; // Clear MIISCAN
+  indir(MICMDH) = 0; // Can this line be deleted?
+  return;
+}
                                           
                                     
                                              
