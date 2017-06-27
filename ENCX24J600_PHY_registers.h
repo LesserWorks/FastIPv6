edda03aad1801a69c3b8447bdb501e6390fecf59
LesserWorks <judahlesser@hotmail.com>
2017-06-27 11:22:39 -0600

    Create ENCX24J600_PHY_registers.h

---
+++ ENCX24J600_PHY_registers.h
@@ -0,0 +1,9 @@
+/* PHY registers */
+#define PHCON1 0x00U
+#define PHSTAT1 0x01U
+#define PHANA 0x04U
+#define PHANLPA 0x05U
+#define PHANE 0x06U
+#define PHCON2 0x11U
+#define PHSTAT2 0x1BU
+#define PHSTAT3 0x1FU
