68a8f30687bb0c8748b8b7cf7ea2bec5ab47526c
LesserWorks <judahlesser@hotmail.com>
2017-06-27 11:40:00 -0600

    Update ENCX24J600_functions.c

--- ENCX24J600_functions.c
+++ ENCX24J600_functions.c
@@ -1,1 +1,15 @@
-extern uint16_t ReadPHY
+extern uint16_t ReadPHY(uint8_t PHYaddress)
+{
+  indir(MIREGADRL) = PHYaddress;
+  indir(MIREGADRH) = 1U;
+  indir(MICMDL) = 1 << MIIRD;
+  indir(MICMDH) = 0; // Can this line be deleted?
+  while(indir(MISTATL) & (1 << BUSY));
+  indir(MICMDL) = 0;
+  indir(MICMDH) = 0;
+  return (((uint16_t)indir(MIIRDH)) << 8) | (indir(MIIRDL);
+                                             }
+                                             
+                                          
+                                    
+                                             
