f564ba10b104765ba497e6e7127a3e8d442e105f
LesserWorks <judahlesser@hotmail.com>
2017-06-27 12:38:43 -0600

    Update FastIPv6.c

--- FastIPv6.c
+++ FastIPv6.c
@@ -1,14 +1,18 @@
 #include <FastIPv6.h>
+#include <avr/io.h>
 #ifdef USE_ENC28J60
   #include <ENC28J60_functions.h>
   #include <ENC28J60_registers.h>
-#else
+#elif defined(ENCX24J600_SPI)
   #include <ENCX24J600_functions.h>
-  #include <ENCX24J600_registers.h>
+  #include <ENCX24J600_SPI_registers.h>
+#elif defined(ENCX24J600_PARALLEL)
+  #include <ENCX24J600_PSP_registers.h>
 #endif /* USE_ENC28J60 */
-#ifdef
-extern void solicitRouter(void) // Visible to other files
-{
-  return;
-}
-// Put local functions with static prototype and function here.
+#ifdef USE_ENC28J60
+  // put functions definitions for ENC28J60 here
+#elif defined(ENCX24J600_SPI)
+  // put function definitions for X24J600 SPI here
+#elif defined(ENCX24J600_PARALLEL)
+  // put function definitions for X24J600 PSP here
+#endif /* USE_ENC28J60 */
