12bb6dd6d3f26e0e88fa6f471583b889cdf76b10
LesserWorks <judahlesser@hotmail.com>
2017-07-09 14:47:51 -0600

    Update ENC28J60_functions.c

--- ENC28J60_functions.c
+++ ENC28J60_functions.c
@@ -1,10 +1,15 @@
 #include <FastIPv6_config.h>
 #include <ENC28J60_functions.h>
 #include <ENC28J60_registers.h>
+
 extern void WriteReg(uint8_t registerName, uint8_t data)
 {
   SS_low();
-  SerialTX(
+  SerialTX(WCR(registerName));
+  SerialTX(data);
+  SerialTXend();
+  SS_high();
+  return;
 }
 extern uint8_t ReadReg(uint8_t registerName)
 {
@@ -12,9 +17,16 @@ extern uint8_t ReadReg(uint8_t registerName)
   SerialTX(RCR(registerName));
   SerialRX(uint8_t trashByte);
   SerialRXend(uint8_t readData);
-  SS_high;
+  SS_high();
   return readData;
 }
 extern uint8_t ReadRegDelayed(uint8_t registerName)
-    {
-      gfjg
+{
+  SS_low();
+  SerialTX(RCR(registerName));
+  SerialRX(uint8_t trashByte);
+  SerialRX(trashByte);
+  SerialRXend(uint8_t readData);
+  SS_high();
+  return readData;
+}
