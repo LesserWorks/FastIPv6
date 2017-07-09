5773f508d5c1bba626637c75d8693d72d5824324
LesserWorks <judahlesser@hotmail.com>
2017-07-09 14:28:09 -0600

    Update FastIPv6_config.h

--- FastIPv6_config.h
+++ FastIPv6_config.h
@@ -4,4 +4,8 @@
 //#define USE_ENC28J60
 //#define USE_ENCX24J600_SPI
 //#define USE_ENCX24J600_PARALLEL
+//#define SS_PORT PORTD
+//#define SS_PIN_NUM 5
+//#define RESET_PORT PORTD
+//#define RESET_PIN_NUM 6
 #endif /* FAST_IPv6_CONFIG_H */
