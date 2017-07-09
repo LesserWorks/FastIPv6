f305ea04bdee5a1abe9315a8cf1a5227513a65dc
LesserWorks <judahlesser@hotmail.com>
2017-07-09 15:36:26 -0600

    Update ENC28J60_functions.h

--- ENC28J60_functions.h
+++ ENC28J60_functions.h
@@ -1,5 +1,25 @@
 #ifndef ENC28J60_FUNCTIONS_H // Include guard
 #define ENC28J60_FUNCTIONS_H
-extern void WriteReg(uint8_t registerName, uint8_t data);
-extern uint8_t ReadReg(uint8_t registerName);
+extern void WriteReg(const uint8_t registerName, const uint8_t data);
+
+extern uint8_t ReadReg(const uint8_t registerName);
+
+extern uint8_t ReadRegDelayed(const uint8_t registerName);
+
+extern void SetRegBit(const uint8_t registerName, const uint8_t data);
+
+extern void ClearRegBit(const uint8_t registerName, const uint8_t data);
+
+extern void WritePHY(const uint8_t registerName, const uint8_t dataH, const uint8_t dataL);
+
+extern uint16_t ReadPHY(const uint8_t registerName);
+
+extern void StartPHYscan(const uint8_t registerName);
+
+extern uint8_t ReadPHYscan(const uint8_t whichByte);
+
+extern void StopPHYscan(void);
+
+extern void IPv6reset(const uint8_t resetType);
+
 #endif /* ENC28J60_FUNCTIONS_H */
