86823e2ee6d33a4682445dee50dee3ea87b5c860
LesserWorks <judahlesser@hotmail.com>
2017-07-09 14:41:16 -0600

    Update ENC28J60_functions.c

--- ENC28J60_functions.c
+++ ENC28J60_functions.c
@@ -3,7 +3,18 @@
 #include <ENC28J60_registers.h>
 extern void WriteReg(uint8_t registerName, uint8_t data)
 {
+  SS_low();
+  SerialTX(
 }
 extern uint8_t ReadReg(uint8_t registerName)
 {
+  SS_low();
+  SerialTX(RCR(registerName));
+  SerialRX(uint8_t trashByte);
+  SerialRXend(uint8_t readData);
+  SS_high;
+  return readData;
 }
+extern uint8_t ReadRegDelayed(uint8_t registerName)
+    {
+      gfjg
